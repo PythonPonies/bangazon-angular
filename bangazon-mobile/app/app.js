14f65c6ace26c008a2596a93bcb6c853d4b8c297
Ikechi <consumerike@gmail.com>
2017-03-07 10:46:55 -0600

    added app.js routes and rootFactory

--- bangazon-mobile/app/app.js
+++ bangazon-mobile/app/app.js
@@ -1,3 +1,62 @@
 'use strict';
 
-let app = angular.module("Bangazon-Mobile", ["ngRoute"]);
+let app = angular.module("Bangazon-Mobile", ["ngRoute"])
+    .constant('apiUrl', "http://localhost:8000");
+
+    angular.module('Bangazon-Mobile').config(
+    [
+        '$httpProvider',
+        '$routeProvider',
+        function($httpProvider, $routeProvider) {
+
+          $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+          $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+          $httpProvider.defaults.withCredentials = true;
+
+          $routeProvider
+            .when('/', {
+              controller: 'HomeController',
+              templateUrl: 'Bangazon-Mobile/index.html'
+            })
+            .when('/product-detail', {
+              controller: 'ProductDetailController',
+              templateUrl: 'Bangazon-Mobile/view/product-detail.html'
+            })
+            .when('/login', {
+              controller: 'LoginController',
+              templateUrl: 'Bangazon-Mobile/view/login.html'
+            })
+            .when('/sell-product', {
+              controller: 'SellProductController',
+              templateUrl: 'Bangazon-Mobile/view/sell-product.html'
+            })
+            .when('/register', {
+              controller: 'RegisterController',
+              templateUrl: 'Bangazon-Mobile/view/register.html'
+            })
+
+            .when('/cart', {
+              controller: 'CartController',
+              templateUrl: 'Bangazon-Mobile/view/cart.html'
+            });
+
+      }
+    ]);
+
+    angular.module('Bangazon-Mobile').factory('RootFactory', [
+      "$http",
+      "apiUrl",
+      ($http, apiUrl) => {
+        const httpGet = $http.get(apiUrl);
+
+        return {
+          getApiRoot () {
+            return httpGet.then(res => res.data)
+          }
+        }
+      }
+    ]);
+
+
+
+
